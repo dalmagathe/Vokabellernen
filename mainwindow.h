ef26f6c0563e0f18ed3baac885f7e9667d0a9ef6
DALMAZIR Agathe <dalmagathe@gmail.com>
2021-08-01 19:55:33 +0200

    Fix check button prb. I didn't call the good function.

--- mainwindow.h
+++ mainwindow.h
@@ -19,27 +19,32 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-
     int fichier(QString germanWord);
 
 private slots:
+    //Menu page
     void on_btnAddWord_clicked();
     void on_btnWordWindow_clicked();
     void on_btnArticleWindow_clicked();
     void on_btnVerbWindow_clicked();
+
+    //Add word page
     void on_btnReturAdd_clicked();
-    void on_btnMenuTranslate_clicked();
     void on_btnAdd_clicked();
+
+    //Guess translation page
     void on_radioButton_clicked();
     void on_radioButton_2_clicked();
-
+    void on_btnMenuTranslate_clicked();
     void on_btnCheckTranslate_clicked();
     void on_btnGenerateTranslate_clicked();
 
+    //Guess article page
     void on_btnCheckArticle_clicked();
     void on_btnMenuArticle_clicked();
     void on_btnGenerateNounArticle_clicked();
 
+    //Guess plural page
     void on_btnMenuPlural_clicked();
     void on_btnGenerateNounPlural_clicked();
     void on_btnCheckPlural_clicked();
