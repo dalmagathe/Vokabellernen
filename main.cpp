a07233f1b562a92a455e3a126ddb00c4fbcf8d0d
DALMAZIR Agathe <dalmagathe@gmail.com>
2021-08-11 17:29:49 +0200

    Removal of the check of the BDD opening in tabs.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include "iostream"
 
 #include <QApplication>
 
@@ -7,6 +8,12 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    if (w.isActiveWindow())
+    {
+        std::cout<<"OPEN"<<std::endl;
+        w.openBDD();
+    }
     return a.exec();
 
 }
